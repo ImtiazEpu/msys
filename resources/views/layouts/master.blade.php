28eb4bbd413cf0840466e9eb298dad1af994659d
Imtiaz Epu <imtiazepu@programmer.net>
2019-09-08 02:38:42 +0600

    template mastaring

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- Favicon -->
+    <link href="{{asset('img/brand/favicon.png')}}" rel="icon" type="image/png">
+    <link type="text/css" rel="stylesheet" href="{{ mix('css/app.css') }}">
+</head>
+
+<body class="bg-default">
+@yield('content')
+<!--   Core   -->
+<script src="{{mix('js/app.js')}}"></script>
+<script src="//cdn.trackjs.com/agent/v3/latest/t.js"></script>
+<script>
+    window.TrackJS &&
+    TrackJS.install({
+        token: "ee6fab19c5a04ac1a32a645abde4613a",
+        application: "argon-dashboard-free"
+    });
+</script>
+</body>
+
+</html>
