33196714e8990d47382a4fe31d2ac7833ae26377
Imtiaz Epu <imtiazepu@programmer.net>
2019-09-09 12:19:38 +0600

    Seed the roles and users tables

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
@@ -11,6 +14,32 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        User::truncate();
+        DB::table('role_user')->truncate();
+
+        $adminRole = Role::where('name', 'admin')->first();
+        $authorRole = Role::where('name', 'author')->first();
+        $userRole = Role::where('name', 'user')->first();
+
+        $admin = User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@msys.com',
+            'password' => Hash::make('admin@7704')
+        ]);
+        $author = User::create([
+            'name' => 'Author User',
+            'email' => 'author@msys.com',
+            'password' => Hash::make('author@7704')
+        ]);
+        $user = User::create([
+            'name' => 'User',
+            'email' => 'user@msys.com',
+            'password' => Hash::make('user@7704')
+        ]);
+
+        $admin->roles()->attach($adminRole);
+        $author->roles()->attach($authorRole);
+        $user->roles()->attach($userRole);
+
     }
 }
